97ee4635c36a7e3eb26298bf719849a59aabc5ea
Sally Crawford <sally.crawford@gmail.com>
2019-04-08 13:36:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     return "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
+  elsif (int % 3 == 0 && int % 5 == 0)
     return "FizzBuzz"
   else
     return "nil"
